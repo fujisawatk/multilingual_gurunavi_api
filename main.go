b7a89b7216df39776bcc8657318cd396dfd1b392
fujisawatk <fuji19880707@gmail.com>
2021-04-05 15:31:41 +0900

    get external API response #1

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"multilingual_gurunavi_api/api"
 	"multilingual_gurunavi_api/config"
 	"net/http"
 )
 
 func main() {
 	config.EnvLoad()
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", api.HandleRestsGet)
 	http.ListenAndServe(":8000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, config.GNAVIURL, "\n", config.GNAVIID)
 }
