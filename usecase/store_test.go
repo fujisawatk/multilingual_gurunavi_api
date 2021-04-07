87396ce76656ae635056233264220ca4c4aa0ab4
fujisawatk <fuji19880707@gmail.com>
2021-04-07 22:32:37 +0900

    add usecase test #19

--- usecase/store_test.go
+++ usecase/store_test.go
@@ -1,9 +1,10 @@
-package persistence_test
+package usecase_test
 
 import (
 	"log"
 	"multilingual_gurunavi_api/config"
 	"multilingual_gurunavi_api/infrastructure/persistence"
+	"multilingual_gurunavi_api/usecase"
 	"net/http"
 	"os"
 	"testing"
@@ -20,7 +21,7 @@ func TestMain(m *testing.M) {
 }
 
 func EnvLoad() {
-	err := godotenv.Load("../../.env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,16 +30,16 @@ func EnvLoad() {
 	config.GNAVIID = os.Getenv("KEY_ID")
 }
 
-func TestGnaviRequest(t *testing.T) {
+func TestGetStores(t *testing.T) {
 	tests := []struct {
 		testCase     string
-		lang         string
+		langs        []string
 		wantSliceLen int
 		wantErr      bool
 	}{
 		{
-			testCase:     "ぐるなびの多言語版レストラン検索から指定の言語に関連する店舗情報を取得できること",
-			lang:         "ja",
+			testCase:     "指定の言語が１つの場合、10個の要素を返すこと",
+			langs:        []string{"ja"},
 			wantSliceLen: 10,
 			wantErr:      false,
 		},
@@ -46,41 +47,26 @@ func TestGnaviRequest(t *testing.T) {
 
 	// go-vcr のレコーダを生成
 	// 保存済みの通信内容からモック化
-	r, _ := recorder.New("../../utils/test_data/gnavi_ja")
+	r, _ := recorder.New("../utils/test_data/gnavi_ja")
 	defer r.Stop()
 
-	//
 	customHTTPClient := &http.Client{
 		Transport: r,
 	}
 
 	sr := persistence.NewStorePersistence(customHTTPClient)
+	su := usecase.NewStoreUsecase(sr)
 
 	for _, tt := range tests {
 		t.Run(tt.testCase, func(t *testing.T) {
-			res, err := sr.GnaviRequest(tt.lang)
+			res, err := su.GetStores(tt.langs)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("storePersistence.GnaviRequest() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("storeUsecase.GetStores() error = %v, wantErr %v", err, tt.wantErr)
 			}
 
-			if len(res.Rest) != tt.wantSliceLen {
-				t.Errorf("storePersistence.GnaviRequest() = %v, want %v", len(res.Rest), tt.wantSliceLen)
+			if len(res) != tt.wantSliceLen {
+				t.Errorf("storeUsecase.GetStores() = %v, want %v", len(res), tt.wantSliceLen)
 			}
 		})
-	}
-}
-
-func BenchmarkGnaviRequest(b *testing.B) {
-	r, _ := recorder.New("./fixtures/gnavi_ja")
-	defer r.Stop()
-
-	customHTTPClient := &http.Client{
-		Transport: r,
-	}
-	sr := persistence.NewStorePersistence(customHTTPClient)
-	// ここから計測開始
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		sr.GnaviRequest("ja")
 	}
 }
